097ad7e30d1a915344ef742e1fe831305fd7c089
Hajk <tunczorx@gmail.com>
2018-04-12 10:55:19 +0200

    Checking how Interface works. Time for some examples.

---
+++ LearningInterfaceAndAbstractClassV0.1/Bee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningInterfaceAndAbstractClassV0._1
+{
+    public class Bee : IStingPatrol //We have to set up all methods and fields and decalere them as public. Now program should complie properly, even if it does nothing.
+    {
+        public int AlertLevel
+        {
+            get
+            {
+                return 5;
+            }
+        }
+       public int StingerLength
+        {
+            get
+            {
+                int x = 5;
+                return x;
+            }
+            set
+            {
+                int x = 10;
+            }
+        }
+
+       public bool LookForEnemies()
+        {
+            return true;
+        }
+
+        public int SharpenStinger(int Length)
+        {
+            int x = 5;
+            x = Length;
+            return Length;
+        }
+    }
+}
---
+++ LearningInterfaceAndAbstractClassV0.1/IStingPatrol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningInterfaceAndAbstractClassV0._1
+{
+    interface IStingPatrol // Created my first interface, checking how it should be used to work properly. 
+    {
+        int AlertLevel { get; }
+        int StingerLength { get; set; }
+        bool LookForEnemies();
+        int SharpenStinger(int Length);
+    }
+}
